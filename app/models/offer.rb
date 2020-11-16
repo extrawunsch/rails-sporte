9c7994a21e68498f13ac9870f0dd7109cd4f75f2
Christoph Bohne christoph-bohne@gmx.de <christoph-bohne@gmx.de>
2020-11-16 23:54:07 +0100

    Changed dependancy for models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :host, class_name: 'User'
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
 
   validates :title, :description, :category, :price, :location, :pick_up_at, presence: true
