b87b0ee8c862e359710320c22116df8cfe7bd76e
Niklas Koch <koch.niklas@yahoo.de>
2020-11-17 16:15:58 +0100

    seed with sample category

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@
   User.create(email: 'ramona@gmail.com', password: '123456', name: 'Ramona')
 
   puts 'inside seed'
-  Offer.create(title: 'Renting out Squash raket', description: 'Lirum Larum Loffelstief', category: 'Racket sports', price: '5', location: 'Mecklenburger Straße 91, Berlin', pick_up_at: '2020-12-19 09:00:00', host_id: 1)
-  Offer.create(title: 'Renting out 10kg kettlebell', description: 'Lirum Larum Pumpernickel', category: 'Gym Equipment', price: '15', location: 'Mecklenburger Straße 96, Berlin', pick_up_at: '2020-12-19 20:00:00', host_id: 2)
+  Offer.create(title: 'Renting out Squash raket', description: 'Lirum Larum Loffelstief', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '5', location: 'Mecklenburger Straße 91, Berlin', pick_up_at: '2020-12-19 09:00:00', host_id: 1)
+  Offer.create(title: 'Renting out 10kg kettlebell', description: 'Lirum Larum Pumpernickel', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '15', location: 'Mecklenburger Straße 96, Berlin', pick_up_at: '2020-12-19 20:00:00', host_id: 2)
   Booking.create(begin_on: '17.11.2020', end_on: '20.11.2020', user_id: 2, offer_id: 1)
   Booking.create(begin_on: '20.11.2020', end_on: '25.11.2020', user_id: 1, offer_id: 2)
 
