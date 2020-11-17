5584bc3b0dbf475564892db4e616033189e1d0ff
Niklas Koch <koch.niklas@yahoo.de>
2020-11-17 17:29:19 +0100

    index done and tested, ready for beautify

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,9 @@
 class OffersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
 
   def index
-    @offers = Offer.all
+
+    @offers = policy_scope(Offer)
   end
 
 end
