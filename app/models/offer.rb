7aaae663554c95a7ddafbd4b0a2af36e5a4beee4
Niklas Koch <koch.niklas@yahoo.de>
2020-11-17 16:12:31 +0100

    added categories from list

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,6 +4,5 @@ class Offer < ApplicationRecord
   has_many :users, through: :bookings
 
   validates :title, :description, :category, :price, :location, :pick_up_at, presence: true
+  validates :category, inclusion: { in: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'] }
 end
-
-
