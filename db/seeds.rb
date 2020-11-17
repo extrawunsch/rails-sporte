6c744ada83e957d50c61dccd52e732a4998f031b
Niklas Koch <koch.niklas@yahoo.de>
2020-11-17 15:13:21 +0100

    ready for getting deployed (we think) :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  puts 'seed begin'
+  # Booking.destroy_all
+  # Offer.destroy_all
+  # User.destroy_all
+
+  puts 'seed begins'
   User.create(email: 'niklas@gmail.com', password: '123456', name: 'Niklas')
+  User.create(email: 'ramona@gmail.com', password: '123456', name: 'Ramona')
+
   puts 'inside seed'
   Offer.create(title: 'Renting out Squash raket', description: 'Lirum Larum Loffelstief', category: 'Racket sports', price: '5', location: 'Mecklenburger Straße 91, Berlin', pick_up_at: '2020-12-19 09:00:00', host_id: 1)
-  Booking.create(begin_on: '17.11.2020', end_on: '20.11.2020', user_id: 1, offer_id: 1)
-  puts 'seed ended'
+  Offer.create(title: 'Renting out 10kg kettlebell', description: 'Lirum Larum Pumpernickel', category: 'Gym Equipment', price: '15', location: 'Mecklenburger Straße 96, Berlin', pick_up_at: '2020-12-19 20:00:00', host_id: 2)
+  Booking.create(begin_on: '17.11.2020', end_on: '20.11.2020', user_id: 2, offer_id: 1)
+  Booking.create(begin_on: '20.11.2020', end_on: '25.11.2020', user_id: 1, offer_id: 2)
+
+  puts "User: #{User.count} created, Offer: #{Offer.count} created, Booking: #{Booking.count} created."
