509f36ff534fac05e601c56fcb0d88b40e0a84b0
Ramona Maltan <ramonamaltan@gmail.com>
2020-11-18 08:51:39 +0100

    added create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:begin_on, :end_on, :offer_id)
   end
 end
