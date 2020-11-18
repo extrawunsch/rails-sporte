f2cab6fb36eb0951b5e49df9ad7702eaccc98e72
Niklas Koch <koch.niklas@yahoo.de>
2020-11-18 15:19:04 +0100

    added cloudinary and picture for new and create and showing it in the index.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,7 @@ class Offer < ApplicationRecord
   belongs_to :host, class_name: 'User'
   has_many :bookings
   has_many :users, through: :bookings
+  has_one_attached :photo
 
   validates :title, :description, :category, :price, :location, :pick_up_at, presence: true
   validates :category, inclusion: { in: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'] }
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,21 @@
   # Offer.destroy_all
   # User.destroy_all
 
+  require "open-uri"
+
+
+
   puts 'seed begins'
   User.create(email: 'niklas@gmail.com', password: '123456', name: 'Niklas')
   User.create(email: 'ramona@gmail.com', password: '123456', name: 'Ramona')
 
+  file = URI.open('https://images.unsplash.com/photo-1579952363873-27f3bade9f55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+  offer =   Offer.new(title: 'Renting out Squash raket', description: 'Lirum Larum Loffelstief', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '5', location: 'Mecklenburger Straße 91, Berlin', pick_up_at: '2020-12-19 09:00:00', host_id: 1)
+  offer.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  offer.save
   puts 'inside seed'
-  Offer.create(title: 'Renting out Squash raket', description: 'Lirum Larum Loffelstief', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '5', location: 'Mecklenburger Straße 91, Berlin', pick_up_at: '2020-12-19 09:00:00', host_id: 1)
-  Offer.create(title: 'Renting out 10kg kettlebell', description: 'Lirum Larum Pumpernickel', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '15', location: 'Mecklenburger Straße 96, Berlin', pick_up_at: '2020-12-19 20:00:00', host_id: 2)
+ #Offer.create(title: 'Renting out Squash raket', description: 'Lirum Larum Loffelstief', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '5', location: 'Mecklenburger Straße 91, Berlin', pick_up_at: '2020-12-19 09:00:00', host_id: 1, photo:)
+ #Offer.create(title: 'Renting out 10kg kettlebell', description: 'Lirum Larum Pumpernickel', category: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'].sample, price: '15', location: 'Mecklenburger Straße 96, Berlin', pick_up_at: '2020-12-19 20:00:00', host_id: 2)
   Booking.create(begin_on: '17.11.2020', end_on: '20.11.2020', user_id: 2, offer_id: 1)
   Booking.create(begin_on: '20.11.2020', end_on: '25.11.2020', user_id: 1, offer_id: 2)
 
