a518115cf369d39a3af15997c9c9fbb2e3c65d7a
Agnès de Briey <debrieyagnes@gmail.com>
2020-11-19 13:34:48 +0100

    Map appears on the screen

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,11 +26,11 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initMapbox();
   initUpdateNavbarOnScroll();
 });
