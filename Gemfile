bbecadb4023c8735b03b67d0b096d9ce36b7eabe
Agnès de Briey <debrieyagnes@gmail.com>
2020-11-19 12:17:38 +0100

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -25,8 +25,6 @@ gem 'turbolinks', '~> 5'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.16.0'
 
-
-
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_231450) do
+ActiveRecord::Schema.define(version: 2020_11_19_103620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,15 +47,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_231450) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
-  end
-
   create_table "offers", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -66,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_11_18_231450) do
     t.bigint "host_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["host_id"], name: "index_offers_on_host_id"
   end
 
