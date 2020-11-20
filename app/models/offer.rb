4005f9345e2e13f21497d6647c426b5681abd214
Agnès de Briey <debrieyagnes@gmail.com>
2020-11-20 02:16:11 +0100

    Map with the :will_save_change_to_location?

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -8,5 +8,5 @@ class Offer < ApplicationRecord
   validates :category, inclusion: { in: ['ball sports', 'racket sports', 'gym equipment', 'water sports', 'winter sports', 'biking', 'fighting sports', 'yoga equipment', 'other'] }
 
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
